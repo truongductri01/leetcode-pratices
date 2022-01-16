9ed89ff32c51e302e8639754b84247dd8f206de8
Tri Truong <truongductri01@gmail.com>
2022-01-15 21:33:57 -0500

    update permutation

--- Google/Google_Interview/2_Arrays_and_Strings/Medium/Next_Permutation/Solution.java
+++ Google/Google_Interview/2_Arrays_and_Strings/Medium/Next_Permutation/Solution.java
@@ -1,12 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
+    public void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i ++;
+            j --;
+        }
+    }
     public void nextPermutation(int[] nums) {
         /*
         596 - 659
         1. Determine if the next permutation is available
             - available if there is a contiguous subarray which is sorted
-            - if the sorted position happends at index i => after swapping the element with the correct element, sort the subarray starting at i+1 to the end
+            - if the sorted position happends at index i => after swapping the element with the correct element, reverse the subarray starting at i+1 to the end
+                - because the part after the sorted position is still sorted but in descending order
             - when the sorted part appears, we need to find the smallest element which is still larger then the element at the position we are working with
                 - find that element by traversing the array backwards until we find the element which is larger than the current element
                 - because the sorted situation happens at index i, the subarray from i+1 to the end of the array is sorted by in descending order => traversing backward makes sure we find the smallest solution
@@ -26,13 +35,13 @@ class Solution {
                     int temp = nums[i];
                     nums[i] = nums[smallestElementLargerThanTheCurrentOneIdx];
                     nums[smallestElementLargerThanTheCurrentOneIdx] = temp;
-                    Arrays.sort(nums, i + 1, N);
+                    reverse(nums, i+1);
                     hasSwapped = true;
                     break;
                 }
             }
             if (!hasSwapped) {
-                Arrays.sort(nums);
+                reverse(nums, 0);
             }
         }
     }
