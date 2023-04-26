3c1e38a85dc6578bf4a0cda47e69e95a22269251
truongductri01 <58579187+truongductri01@users.noreply.github.com>
2023-04-26 02:02:12 -0400

    Update ProductOfArrayExceptSelf.java

--- Neetcode_150/Arrays&Hashing/ProductOfArrayExceptSelf.java
+++ Neetcode_150/Arrays&Hashing/ProductOfArrayExceptSelf.java
@@ -10,43 +10,38 @@ class Solution {
         => how about counting 0, if there is more than 1 0, all values will be 0
             otherwise, just calculate the sum without multiplying with 0?
 
+        
+        Cannot use division also!!!
+
         Runtime: O(n)
+
+
+        Can we utilize prefix and suffix hint in the constraints?
+        Start from the left to set up the prefix
+        Start from the right to set up the suffix
+        Then multiply prefix and suffix for each position
+        Runtim: O(n)
          */
+        
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
 
-        boolean hasZero = false;
-        boolean hasMultipleZero = false;
+        prefix[0] = 1;
+        suffix[nums.length - 1] = 1;
 
-        for (int n: nums) {
-            if (n == 0) {
-                if (hasZero) {
-                    hasMultipleZero = true;
-                }
-                hasZero = true;
-            }
+        for (int i = 1; i < nums.length; i++) {
+            // for the prefix
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+            // for the suffix
+        }
+        for (int i = nums.length - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * nums[i + 1];
         }
 
-        if (hasMultipleZero) {
-            return new int[nums.length];
-        } else {
-            int mult = 1;
-            for (int n: nums) {
-                if (n != 0) {
-                    mult *= n;
-                }
-            }
-
-            int[] result = new int[nums.length];
-            for (int i = 0; i < nums.length; i++) {
-                int n = nums[i];
-                if (n != 0) {
-                    int value = 0;
-                    if (!hasZero) value = mult / n;
-                    result[i] = value;
-                } else {
-                    result[i] = mult;
-                }
-            }
-            return result;
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = prefix[i] * suffix[i];
         }
+        return result;
     }
 }
